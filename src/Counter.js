648df50c173a780d3d382d215e4e6616ee76ffe6
LeeYunJae <yunjea0312@naver.com>
2019-05-10 22:09:37 +0900

    Add tsconfig.json and Update mobx
    Add tsconfig.json and Update mobx

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import { decorate, observable, action } from 'mobx';
+import { observable, action } from 'mobx';
 import { observer } from 'mobx-react';
 
-// @observer
+@observer
 class Counter extends Component {
-  number = 0;
+  @observable number = 0;
 
-  
+  @action
   increase = () => {
     this.number++;
   }
 
+  @action
   decrease = () => {
     this.number--;
   }
@@ -26,10 +27,10 @@ class Counter extends Component {
   }
 }
 
-decorate(Counter, {
-  number: observable,
-  increase: action,
-  decrease: action
-})
+// decorate(Counter, {
+//   number: observable,
+//   increase: action,
+//   decrease: action
+// })
 
 export default observer(Counter);
